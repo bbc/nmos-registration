156b3a40218314b4e9b866db07fd7f815e033e2a
Peter Brightwell <peter.brightwell@bbc.co.uk>
2017-10-05 19:26:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# NMOS Registry Registration API Service
+
+Provides a service implementing the NMOS Registration API, specified in AMWA IS-04.
+
+The API will present itself at [http://localhost:8235/x-nmos/registration/].
+
+## Installing with Python
+
+Before installing this library please make sure you have installed the [NMOS Common Library](https://github.com/bbc/nmos-common), on which this API depends. The registration API also requires [etcd](https://github.com/coreos/etcd) to be installed. For debain distributions this can be installed using apt:
+
+```
+sudo apt-get install etcd
+
+```
+
+Once all dependencies are satisfied run the following commands to install the API:
+
+```
+pip install setuptools
+sudo python setup.py install
+```
+
+## Running the Registration API
+
+If you wish the Registration API to run automatically as a service a symbolic link must be added to the system. In order to do this, run the following command in bash.
+
+```bash
+    ln -s /lib/systemd/system/nmosregistration.service /etc/systemd/system/multi-user.target.wants/nmosregistration.service
+```
+
+### Non-blocking
+
+Run the following script to start the Registration API in a non-blocking manner, and then stop it again at a later point:
+
+```Python
+    from nmosregistration.registryaggregatorservice import RegistryAggregatorService
+
+    service = RegistryAggregatorService()
+    service.start()
+    
+    # Do something else until ready to stop
+    
+    service.stop()
+```
+
+### Blocking
+
+It is also possible to run Registration API in a blocking manner:
+
+```python
+from nmosregistration.registryaggregatorservice import RegistryAggregatorService
+
+service = RegistryAggregatorService()
+service.run() # Runs forever
+```
+
+## Tests
+
+Unit tests are provided.  Currently these have hard-coded dummy/example hostnames, IP addresses and UUIDs.  You will need to edit the Python files in the test/ directories to suit your needs and then "make test".
+
+## Debian Packaging
+
+Debian packaging files are provided for internal BBC R&D use.
+These packages depend on packages only available from BBC R&D internal mirrors, and will not build in other environments. For use outside the BBC please use python installation method.
+
---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+#
+# Copyright 2017 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup
+from distutils.version import LooseVersion
+import os
+import sys
+
+def check_packages(packages):
+    failure = False
+    for python_package, package_details in packages:
+        try:
+            __import__(python_package)
+        except ImportError as err:
+            failure = True
+            print "Cannot find", python_package,
+            print "you need to install :", package_details
+
+    return not failure
+
+def check_dependencies(packages):
+    failure = False
+    for python_package, dependency_filename, dependency_url in packages:
+        try:
+            __import__(python_package)
+        except ImportError as err:
+            failure = True
+            print
+            print "Cannot find", python_package,
+            print "you need to install :", dependency_filename
+            print "... originally retrieved from", dependency_url
+
+    return not failure
+
+def is_package(path):
+    return (
+        os.path.isdir(path) and
+        os.path.isfile(os.path.join(path, '__init__.py'))
+        )
+
+def find_packages(path, base="" ):
+    """ Find all packages in path """
+    packages = {}
+    for item in os.listdir(path):
+        dir = os.path.join(path, item)
+        if is_package( dir ):
+            if base:
+                module_name = "%(base)s.%(item)s" % vars()
+            else:
+                module_name = item
+            packages[module_name] = dir
+            packages.update(find_packages(dir, module_name))
+    return packages
+
+packages = find_packages(".")
+package_names = packages.keys()
+
+packages_required = [
+    "nmoscommon"
+]
+
+deps_required = []
+
+setup(name = "nmosregistration",
+      version = "0.1.0",
+      description = "nmos registration API",
+      url='www.nmos.tv',
+      author='Peter Brightell',
+      author_email='peter.brightwell@bbc.co.uk',
+      license='Apache 2',
+      packages = package_names,
+      package_dir = packages,
+      install_requires = packages_required,
+      scripts = [
+                ],
+      data_files=[
+          ('/lib/systemd/system', ['lib/systemd/system/nmosregistration.service']),
+          ('/usr/bin', ['bin/nmosregistration'])
+      ],
+      long_description = """
+Provides a service implementing the NMOS Registration API
+"""
+      )
