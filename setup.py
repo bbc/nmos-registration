750cd8046a103c67a7606cee7978ceca2a15edd2
Andrew Bonney <andrew.bonney@bbc.co.uk>
2018-08-03 09:31:44 +0100

    stdeb: add missing dependencies

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ def find_packages(path, base=""):
 packages = find_packages(".")
 package_names = packages.keys()
 
+# REMEMBER: If this list is updated, please also update stdeb.cfg and the RPM specfile
 packages_required = [
     "gevent>=1.2.2",
     "nmoscommon>=0.6.0",
@@ -55,7 +56,7 @@ packages_required = [
 ]
 
 setup(name="registryaggregator",
-      version="0.2.2",
+      version="0.2.3",
       description="nmos registration API",
       url='www.nmos.tv',
       author='Peter Brightwell',
