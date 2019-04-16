c7b599c6e4b88940bbcd4240b50df7181075143a
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2019-04-16 14:33:00 +0100

    version bump

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ packages_required = [
 
 setup(
     name="registryaggregator",
-    version="0.6.6",
+    version="0.6.7",
     description="BBC implementation of an AMWA NMOS Registration API",
     url='https://github.com/bbc/nmos-registration',
     author='Peter Brightwell',
